2b54e4cb792bdb6929b231c7f80fbc7f01a2b405
Mehul Patel <mspatel.it@spcevng.ac.in>
2024-05-25 08:59:57 +0530

    changed logo in tab

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.o634s5qfoao"
+    "revision": "0.fm8deiamrlo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
